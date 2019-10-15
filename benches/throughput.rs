568f760b7c7f9138bade71d6e3683ef9ed0c0c38
Josh Hejna <josh.hejna@gmail.com>
2019-10-14 21:19:09 -0700

    Cargo fmt

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -1,19 +1,21 @@
-use criterion::BenchmarkId;
-use criterion::Criterion;
+use criterion::black_box;
 use criterion::criterion_group;
 use criterion::criterion_main;
-use criterion::black_box;
+use criterion::BenchmarkId;
+use criterion::Criterion;
 use criterion::Throughput;
-
 
 use std::convert::TryInto;
 
-use std::thread;
 use lfq::*;
+use std::thread;
 
 use std::time::Instant;
 
-use std::sync::{Arc, atomic::{AtomicBool, Ordering::SeqCst}};
+use std::sync::{
+    atomic::{AtomicBool, Ordering::SeqCst},
+    Arc,
+};
 
 #[derive(Default)]
 struct DummyThreadManager {
@@ -24,15 +26,18 @@ impl DummyThreadManager {
     fn add_thread(&mut self, f: impl FnOnce(Arc<AtomicBool>) + Send + 'static) {
         let a = Arc::new(AtomicBool::new(true));
         let b = a.clone();
-        let h = thread::spawn(move || {
-            f(b)
-        });
-        self.handles.push((h,a));
+        let h = thread::spawn(move || f(b));
+        self.handles.push((h, a));
     }
 
     fn join_all(mut self) {
-        (&mut self).handles.iter_mut().for_each(|(_h, b)| b.store(false, SeqCst));
-        self.handles.into_iter().for_each(|(h, _b)| h.join().unwrap());
+        (&mut self)
+            .handles
+            .iter_mut()
+            .for_each(|(_h, b)| b.store(false, SeqCst));
+        self.handles
+            .into_iter()
+            .for_each(|(h, _b)| h.join().unwrap());
     }
 }
 
@@ -41,7 +46,6 @@ struct DataDummy(u64, f64, f64, f64);
 const DATA_DEFAULT: DataDummy = DataDummy(1233123, 30912831.132213, -12931.123, 123.98);
 
 fn single_producer(c: &mut Criterion) {
-
     let mut g = c.benchmark_group("My Group");
 
     const QSIZE: usize = 128;
@@ -50,39 +54,42 @@ fn single_producer(c: &mut Criterion) {
     g.throughput(Throughput::Elements(ITEMS as u64));
 
     for consumers in 1..=15 {
-        g.bench_with_input(BenchmarkId::new("Multiply", consumers), &consumers,
-            |b, consumers| b.iter_custom(|iters| {
-                eprintln!("iters {}", iters);
-                let q = QueueClient::<DataDummy>::new_queue(QSIZE);
-                let mut tm = DummyThreadManager::default();
-                for _ in 0..*consumers {
-                    let mut qp = q.clone();
-                    tm.add_thread(move |b| {
-                        while b.load(SeqCst) {
-                            for _ in 0..100 {
-                                black_box(qp.next());
+        g.bench_with_input(
+            BenchmarkId::new("Multiply", consumers),
+            &consumers,
+            |b, consumers| {
+                b.iter_custom(|iters| {
+                    eprintln!("iters {}", iters);
+                    let q = QueueClient::<DataDummy>::new_queue(QSIZE);
+                    let mut tm = DummyThreadManager::default();
+                    for _ in 0..*consumers {
+                        let mut qp = q.clone();
+                        tm.add_thread(move |b| {
+                            while b.load(SeqCst) {
+                                for _ in 0..100 {
+                                    black_box(qp.next());
+                                }
                             }
-                        }
-                    })
-                }
-                let start = Instant::now();
-                for _i in 0..iters {
-                    for _ in 0..ITEMS {
-                        q.push(DATA_DEFAULT);
+                        })
                     }
-
-                }
-                let el = start.elapsed();
-                tm.join_all();
-                el
-            })
+                    let start = Instant::now();
+                    for _i in 0..iters {
+                        for _ in 0..ITEMS {
+                            q.push(DATA_DEFAULT);
+                        }
+                    }
+                    let el = start.elapsed();
+                    tm.join_all();
+                    el
+                })
+            },
         );
     }
 
     g.finish()
 }
 
-criterion_group!{
+criterion_group! {
     name = benches;
     config = Criterion::default();
     targets = single_producer
