795426a0c5e33a7cd6c32c2e250f5871acfb9911
Josh Hejna <josh.hejna@gmail.com>
2019-11-12 12:49:21 -0800

    Import and build aos_sync

---
+++ aos_sync/build.rs
@@ -0,0 +1,37 @@
+use bindgen;
+use cc;
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    cc::Build::new()
+        .file("cpp/aos/ipc_lib/aos_sync.cc")
+        .file("cpp/aos/libc/aos_strerror.cc")
+        .include("cpp")
+        .cpp(true)
+        .flag("-std=c++14")
+        .warnings_into_errors(true)
+        .warnings(true)
+        .flag("-Wno-unused-variable")
+        // .warnings_extra(true)
+        .static_flag(true)
+        .cargo_metadata(true)
+        .compile("aos_sync");
+
+    println!("cargo:rerun-if-changed=cpp/aos/ipc_lib/aos_sync.h");
+    println!("cargo:rerun-if-changed=cpp/aos_sync.cc");
+
+    let bindings = bindgen::Builder::default()
+        .header("cpp/aos/ipc_lib/aos_sync.h")
+        .whitelist_type("aos_.*")
+        .whitelist_function("mutex_.*")
+        .whitelist_function("futex_.*")
+        .whitelist_function("condition_.*")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
